7c7283c69589ced08acf61e4519095a868a8b844
munoz138255 <munoz.138255@e.unavarra.es>
2025-04-01 08:06:39 +0200

    Cambio en index, debuggeando con console.log

--- src/component/listItem/index.js
+++ src/component/listItem/index.js
@@ -7,7 +7,7 @@ class ListItem extends Component {
         const { title, desc } = this.props;
 
         if(!title){
-            console.log("No hay título");
+            console.log("No  hay título");
             return null;
         }
 
